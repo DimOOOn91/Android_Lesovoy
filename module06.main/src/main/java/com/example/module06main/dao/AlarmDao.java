5040ac56d6863db6e2b1f7185c6684765ac59dbb
Dmytro Lisovyi <dimalis.mail@gmail.com>
2017-01-21 17:07:34 +0200

    module 06

---
+++ module06.main/src/main/java/com/example/module06main/dao/AlarmDao.java
@@ -0,0 +1,13 @@
+package com.example.module06main.dao;
+
+import com.example.module06main.entity.Alarm;
+
+import java.util.ArrayList;
+
+public interface AlarmDao {
+
+    Alarm saveAlarm(Alarm alarm);
+    boolean deleteAlarmById(int id);
+    Alarm getById(int id);
+    ArrayList<Alarm> getAll();
+}
